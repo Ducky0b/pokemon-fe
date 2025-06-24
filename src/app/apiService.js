10f9adb9de9d354efe3b96f5bcd9bd61e7aca09d
Ducky0b <vanviet28052002@gmail.com>
2025-06-24 17:06:00 +0700

    fix url api

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./config";
 
 const apiService = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://pokemon-be-tkcu.onrender.com",
 });
 
 apiService.interceptors.request.use(
